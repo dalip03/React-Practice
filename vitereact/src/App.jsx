bed1dcf62bb59ba5f98e3232db3ed3448884e485
dalip03 <dalipdivakar@gmail.com>
2024-05-05 18:21:37 +0530

    create a function

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -5,6 +5,14 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  //  This is a simple button that decrements the count
+  const decrement = ()=> {
+     if(count > 0){
+         setCount(count -1)
+     }
+  }
+
   return (
     <>
       <div  className="container mx-auto p-10 border  bg-green-50 shadow-xl shadow-cyan-500/50" >
@@ -26,10 +34,10 @@ function App() {
 
         </div>
        
-        <div className="text-center p-4">
+        <div className="container text-center p-4 bg-zinc-200 rounded mt-4">
         <h1 className="text-xl mb-4 font-semibold">Counter Value : <span className="font-bold text-green-950">{count}</span> </h1>
-         <button onClick={() =>setCount(count +1)} className="bg-green-400 p-2 rounded-xl  cursor-pointer hover:bg-green-200 hover:text-black mr-1">Increase</button>
-        <button onClick={() =>setCount(count-1)} className="bg-orange-400 p-2 rounded-xl  cursor-pointer hover:bg-orange-200 hover:text-black mr-1">Decrease</button>
+         <button onClick={() =>setCount(count +1)} className="bg-green-400 p-2 rounded-xl  cursor-pointer hover:bg-green-200 hover:text-black mr-1">Increment</button>
+        <button onClick={decrement} className="bg-orange-400 p-2 rounded-xl  cursor-pointer hover:bg-orange-200 hover:text-black mr-1">Decrement</button>
         <button onClick={()=>setCount(0)} className="bg-cyan-400 p-2 rounded-xl  cursor-pointer hover:bg-cyan-200 hover:text-black">Reset to 0</button>
 
         </div>
